2ba117240ea80a8c9acc234886512a5d181d6bf4
L1ll1ies <59169796+L1ll1ies@users.noreply.github.com>
2019-12-30 11:00:50 +0000

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://6f374697.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://6f374697.eu-gb.apigw.appdomain.cloud/features';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
